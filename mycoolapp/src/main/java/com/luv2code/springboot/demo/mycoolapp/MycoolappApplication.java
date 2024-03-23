eb45baa39be9528c2ec5688d07b2230f064b437d
Ducatidark <55323199+Ducatidark@users.noreply.github.com>
2024-03-23 17:57:32 +0100

    commit 7

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MycoolappApplication {
+	private String test;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
