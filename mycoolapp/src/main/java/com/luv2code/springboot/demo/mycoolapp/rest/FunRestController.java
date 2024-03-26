70de8ccf63eededc81ec98447df5e4a2e6278aeb
Ducatidark <55323199+Ducatidark@users.noreply.github.com>
2024-03-26 18:41:26 +0100

    second commit hub2

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -11,6 +11,6 @@ public class FunRestController {
 	}
 	@GetMapping("/git")
 	public String sayGoodBye() {
-		return "Good bye 2";
+		return "Good Bye 2";
 	}
 }
---
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/MyTestInterface.java
@@ -0,0 +1,6 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+public interface MyTestInterface {
+    public void method1();
+    public int method2();
+}
